d24c35f4ea38a5e59dee1222c32180601fac8797
sergio berdiell sanchez <sergiovencillon@gmail.com>
2022-11-16 10:02:21 +0100

    spring_global_config

---
+++ springconfserver/src/main/java/com/springconfserver/SpringconfserverApplication.java
@@ -0,0 +1,13 @@
+package com.springconfserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringconfserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringconfserverApplication.class, args);
+	}
+
+}
---
+++ springconfserver/src/test/java/com/springconfserver/SpringconfserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springconfserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringconfserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
